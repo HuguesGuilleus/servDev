af8a74d28677a72865278f3451ccd5785f3d8ceb
hugues <ghugues@netc.fr>
2020-08-11 12:14:53 +0200

    main.go: Add TLS

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 func init() {
@@ -18,15 +19,36 @@ func init() {
 }
 
 func main() {
-	addrHttp := flag.String("h", ":8000", `L'adresse d'écoute du server HTTP`)
+	addrHttp := flag.String("a", ":8000", `L'adresse d'écoute du server HTTP`)
+	addrHttps := flag.String("s", ":8443", `L'addresse d'écoute HTTPS si il y a une clé et un certificat`)
+	key := flag.String("k", "", "La clé de chiffrement (PEM)")
+	cert := flag.String("c", "", "Le certificat")
 	flag.Parse()
-	root := flag.Arg(0)
+	root, _ := filepath.Abs(flag.Arg(0))
 
 	s := &server{Dir: http.Dir(root)}
 	http.Handle("/", s)
 	http.HandleFunc("/favicon.ico", s.favicon)
 
-	r, _ := filepath.Abs(root)
-	log.Printf("[LISTEN] %s on %s\n", r, *addrHttp)
-	log.Fatal(http.ListenAndServe(*addrHttp, nil))
+	if *key != "" && *cert != "" {
+		go func() {
+			log.Printf("[LISTEN HTTP] %s (redirect)\n", *addrHttp)
+			to := "https://" + *addrHttps
+			if strings.HasPrefix(*addrHttps, ":") {
+				to = "https://localhost" + *addrHttps
+			}
+			log.Fatal(http.ListenAndServe(
+				*addrHttp,
+				http.RedirectHandler(to, http.StatusTemporaryRedirect),
+			))
+		}()
+		go func() {
+			log.Printf("[LISTEN HTTPS] %s on %s\n", root, *addrHttps)
+			log.Fatal(http.ListenAndServeTLS(*addrHttps, *cert, *key, nil))
+		}()
+		select {}
+	} else {
+		log.Printf("[LISTEN HTTP] %s on %s\n", root, *addrHttp)
+		log.Fatal(http.ListenAndServe(*addrHttp, nil))
+	}
 }
